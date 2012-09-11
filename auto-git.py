e13e14c367a781b205e376d51f49d2fa099db9b3
russjohnson09 <russjohnson09@gmail.com>
2012-09-11 19:59:03 -0400

    "update"

---
+++ auto-git.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import subprocess
+    
+    
+    
+def git_default():
+    subprocess.call(['git','add','.'])
+    subprocess.call(['git','commit','-m','"update"'])
+    subprocess.call(['git','push','origin','master'])
+    
+def git_options():
+    pass
+    
+    
+def input():
+    str1 = "Press enter to continue with default settings.\n\
+Type 1 and press enter to use your own settings."
+    x = raw_input(str1)
+    if x==1:
+        git_options()
+    else:
+        git_default()
+        
+        
+        
+if __name__ == "__main__":
+    input()
+    
